e3bfe5972a6e79319c2694f57c3ee1a3b626542b
Marcin Flis <marcin.flis.95@gmail.com>
2018-03-06 08:18:31 +0100

    3 histograms

--- lab1/pattern_extarcors.py
+++ lab1/pattern_extarcors.py
@@ -58,9 +58,32 @@ tmp_path = f"{root_dir}/tmp/output.txt"
 output_file = open(tmp_path, 'r')
 money_amounts = np.asarray(process_file(output_file))
 logbins = np.geomspace(money_amounts.min(), money_amounts.max(), 15)
+
 plt.hist(money_amounts, bins=logbins)
 plt.xscale('log')
 plt.title("Kwoty w wyrokach sądowych 2018")
 plt.xlabel("kwota [zł]")
 plt.ylabel("Częstotliwość")
-plt.show()
+plt.savefig('kwoty-all.png')
+plt.close()
+lesser_amounts = money_amounts[money_amounts < 1000000]
+logbins_lesser = np.geomspace(lesser_amounts.min(), lesser_amounts.max(), 15)
+
+bigger_amounts = money_amounts[money_amounts >= 1000000]
+logbins_bigger = np.geomspace(bigger_amounts.min(), bigger_amounts.max(), 15)
+
+plt.hist(lesser_amounts, bins=logbins_lesser)
+plt.xscale('log')
+plt.title("Kwoty w wyrokach sądowych 2018 mniejsze niż 1 mln zł")
+plt.xlabel("kwota [zł]")
+plt.ylabel("Częstotliwość")
+plt.savefig('kwoty-lesser.png')
+plt.close()
+
+plt.hist(bigger_amounts, bins=logbins_bigger)
+plt.xscale('log')
+plt.title("Kwoty w wyrokach sądowych 2018 większe niż 1 mln zł")
+plt.xlabel("kwota [zł]")
+plt.ylabel("Częstotliwość")
+plt.savefig('kwoty-bigger.png')
+plt.close()
