3cc006737f71547a15fb58630694d59ace73290d
Marcin Flis <marcin.flis.95@gmail.com>
2018-05-12 16:39:31 +0200

    lab8 - refactored groups

--- lab8/dump.py
+++ lab8/dump.py
@@ -41,3 +41,27 @@ content = urllib2.request.urlopen(
     urllib2.Request(url + '/download' + data)).read()
 with open(out_path + os.path.basename(file) + '.ccl', "w") as outfile:
     outfile.write(content)
+    
+
+for sent in first.findall(".//sentence"):
+    toks = sent.findall(".//tok")
+    anns = tok.findall('ann')
+
+    relevant_anns = []
+    for tok in toks:
+        anns = tok.findall('ann')
+        for ann in anns:
+            if int(ann.text) > 0:
+                relevant_anns.append((ann, tok.find('orth').text))
+    channed = sorted(
+        relevant_anns, key=lambda x: (x[0].attrib['chan'], int(x[0].text)))
+    grouped = groupby(
+        channed, key=lambda x: (x[0].attrib['chan'], int(x[0].text)))
+    groups = []
+    uniquekeys = []
+    for k, g in grouped:
+        txt = ' '.join(list(map(itemgetter(1), g)))
+        groups.append((k[0], txt))  # Store group iterator as a list
+        uniquekeys.append(k)
+
+    print(groups)
